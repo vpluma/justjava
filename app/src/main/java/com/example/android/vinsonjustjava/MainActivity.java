8fa9d3bccfdb2361e2ac70ca4752367d3630876d
Vinson Pluma <vpluma@nlmusd.org>
2015-11-15 16:43:40 -0800

    Added createOrderSummary method

--- app/src/main/java/com/example/android/vinsonjustjava/MainActivity.java
+++ app/src/main/java/com/example/android/vinsonjustjava/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.android.vinsonjustjava;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -24,36 +23,50 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + " \nThank you!";
-        displayMessage(priceMessage);
+        /*int price = quantity * 5; */
+        int price = calculatePrice();
+        createOrderSummary(price);
         /*int numberOfCoffees = quantity;
         display(numberOfCoffees);
         displayPrice(numberOfCoffees * 5);*/
+    }
+    /**
+     * Calculates the price of the order based on the quantity & pricePerCup.
+     *
+     * @return the price
+     */
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
+
+    public void createOrderSummary(int price){
+        String priceMessage = "Name: Kaptain Kunal\nQuantity: " + quantity + "\nTotal: $" + price;
+        priceMessage = priceMessage + " \nThank you!";
+        displayMessage(priceMessage);
     }
     /**
      * This method is called when the + button is clicked.
      */
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
     /**
      * This method is called when the - button is clicked.
      */
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int numberOfCoffees) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numberOfCoffees);
     }
 
     /**
